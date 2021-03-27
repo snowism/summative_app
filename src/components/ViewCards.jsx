fec081748bb187874efc59a36bbe7dfb7c4fd935
snowism <snowism23@gmail.com>
2021-03-27 16:00:04 +1300

    test

---
+++ src/components/ViewCards.jsx
@@ -0,0 +1,11 @@
+import React, { Component } from 'react'
+
+export default class ViewCards extends Component {
+    render() {
+        return (
+            <div>
+                
+            </div>
+        )
+    }
+}
