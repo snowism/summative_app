e7618e34e689886ead4e09a783c28558a236a8c0
snowism <snowism23@gmail.com>
2021-03-29 15:32:02 +1300

    some css

---
+++ src/components/SelectBrand.jsx
@@ -0,0 +1,18 @@
+import React, { Component } from 'react'
+
+export default class SelectBrand extends Component {
+    render() {
+        return (
+            <React.Fragment>
+            <div>
+              <h3> Select by brand </h3>
+            </div>
+      <div>
+        <div className="nike">a</div>
+        <div className="nike">b</div>
+        <div className="nike">c</div>
+      </div>
+      </React.Fragment>
+        )
+    }
+}
--- src/components/SingleCard.jsx
+++ src/components/SingleCard.jsx
@@ -10,13 +10,17 @@ export default class SingleCard extends Component {
     
     render() {
         return (
+
+            
+                
             <div className="card">
               
                <p>{this.props.name}</p>
                <p>{this.props.price}</p>
              
-               <p>{this.props.Price}</p>
+              
             </div>
+           
         )
     }
 }
