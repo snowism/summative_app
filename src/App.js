88ce182df3925315fc17ecb8e43009f4f9393a45
snowism <snowism23@gmail.com>
2021-03-30 13:22:44 +1300

    product page layout

--- src/App.js
+++ src/App.js
@@ -1,14 +1,23 @@
 import logo from './logo.svg';
-import './App.css';
-import Wrapper from './components/Wrapper'
+import { Router } from '@reach/router'
 
+import Wrapper from './components/Wrapper'
+import Nike from "./components/Nike"
+
+import './App.css';
 import './scss/Card.scss'
 import './scss/Style.scss'
 
 function App() {
   return (
     <div className="App">
-     <Wrapper/>
+     
+      <Router>
+     <Wrapper path="/"/>
+     <Nike path="/show-nike"/>
+
+     </Router>
+     {/* <Footer/> */}
     </div>
   );
 }
--- src/components/SelectBrand.jsx
+++ src/components/SelectBrand.jsx
@@ -1,4 +1,5 @@
 import React, { Component } from 'react'
+import { Link } from '@reach/router'
 
 export default class SelectBrand extends Component {
     render() {
@@ -7,8 +8,8 @@ export default class SelectBrand extends Component {
             <div>
               <h3> Select by brand </h3>
             </div>
-      <div>
-        <div className="nike">a</div>
+      <div className="select-box">
+        <Link to="/show-nike"><div className="nike">a</div></Link>
         <div className="nike">b</div>
         <div className="nike">c</div>
       </div>
