b556320e947454ff53da8c95a9402085b4b190f2
snowism <snowism23@gmail.com>
2021-03-31 13:12:17 +1300

    landing page

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import logo from './logo.svg';
 import './App.css';
 import Wrapper from './components/Wrapper'
+import Landing from './components/Landing'
 
 import './scss/Card.scss'
 import './scss/Style.scss'
@@ -8,6 +9,7 @@ import './scss/Style.scss'
 function App() {
   return (
     <div className="App">
+      <Landing/>
      <Wrapper/>
     </div>
   );
