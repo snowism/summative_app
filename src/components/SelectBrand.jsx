5b120e2e4ee4312f3066b88da6ecddd7b560a6ea
snowism <snowism23@gmail.com>
2021-04-03 20:02:39 +1300

    brand css

--- src/components/SelectBrand.jsx
+++ src/components/SelectBrand.jsx
@@ -7,9 +7,9 @@ export default class SelectBrand extends Component {
             <React.Fragment>
               <h3 className="title"> Select by brand </h3>
       <div className="select-box">
-        <Link to="/show-nike"><div className="nike"><img src="./images/logo_n.png"/></div></Link>
-        <Link to="/show-nike"><div className="nike"><img src="./images/logo_a.png"/></div></Link>
-        <Link to="/show-nike"><div className="nike"><img src="./images/logo_j.png"/></div></Link>
+        <Link to="/show-nike"><div className="logo-box"><img src="./images/logo_n.png"/></div></Link>
+        <Link to="/show-nike"><div className="logo-box"><img src="./images/logo_a.png"/></div></Link>
+        <Link to="/show-nike"><div className="logo-box"><img src="./images/logo_j.png"/></div></Link>
         
       </div>
       </React.Fragment>
