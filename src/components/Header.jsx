30acdd815d28b5cfc728ae259580b8bb7a0b25c1
Emma Paris <perdanarazif@gmail.com>
2021-04-07 23:11:49 +1200

    header fixed

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -23,14 +23,15 @@ const useStyles = makeStyles((theme) => ({
     },
   },
   search: {
-    position: 'relative',
+    position: 'fixed',
+    right: '3%',
     borderRadius: theme.shape.borderRadius,
     backgroundColor: fade(theme.palette.common.white, 0.15),
     '&:hover': {
       backgroundColor: fade(theme.palette.common.white, 0.25),
     },
     marginLeft: 0,
-    width: '100%',
+    width: '40%',
     [theme.breakpoints.up('sm')]: {
       marginLeft: theme.spacing(1),
       width: 'auto',
