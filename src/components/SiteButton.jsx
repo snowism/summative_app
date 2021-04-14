b259b277b74ad77356a03e24aa55aca09153945a
snowism <snowism23@gmail.com>
2021-04-15 00:57:49 +1200

    btn changed

--- src/components/SiteButton.jsx
+++ src/components/SiteButton.jsx
@@ -1,14 +1,18 @@
 import React, { Component } from 'react'
 import { navigate } from "@reach/router";
 
+
+
+
+
 export default class SiteButton extends Component {
     render() {
         return (
             <div>
-                  <button onClick={this.props.action} >
-          {/* || is a javascript OR */}
+                  <button onClick={this.props.action} style={this.props.style}>
+        
           {this.props.message }   {this.props.icon}
-          {/* "set text via props" */}
+          {/* || is a javascript OR */}  {/* "set text via props" */}
         
 
         </button>
--- src/components/ViewDetails.jsx
+++ src/components/ViewDetails.jsx
@@ -1,112 +1,108 @@
-import React, { Component } from 'react'
+import React, { Component } from "react";
 import Axios from "axios";
 import SiteButton from "./SiteButton";
 import { navigate } from "@reach/router";
 import CommentsWrapper from "./CommentsWrapper";
 import AddComment from "./AddComment";
 
+
+var blackBtnStyle = {
+    width: "120px",
+    height: "26px",
+    background: "black",
+    color: "white",
+    border: "none",
+    borderRadius:"20px",
+}
+
+var whiteBtnStyle = {
+    width: "80px",
+    height: "26px",
+    background: "white",
+    color: "black",
+    border: "none",
+    borderRadius:"20px",
+}
+
 export default class ViewDetails extends Component {
-    constructor (props){
-        super (props);
-        console.log ("ID = ", this.props.location.state.id)
+  constructor(props) {
+    super(props);
+    console.log("ID = ", this.props.location.state.id);
 
-        this.state = {
-            id: this.props.location.state.id,
-            sneaker: {name:"",
-            
-            },
-            totalprice:"",
-        }
-
-        console.log(">>>> ", this.props.location.state.id);
-        console.log(">>>> ", this.state.name);
-    }
-
-    componentDidMount(){
-        Axios.get( `http://localhost:4000/api/sneakers/${this.state.id}`).then (
-            res => {
-                console.table(res);
-             this.setState({sneaker:res.data})
-            }
-        )
-    }
-
-    navigateBack = e => {
-        navigate(-1);
+    this.state = {
+      id: this.props.location.state.id,
+      sneaker: { name: "" },
+      totalprice: "",
     };
 
-    
+    console.log(">>>> ", this.props.location.state.id);
+    console.log(">>>> ", this.state.name);
+  }
 
-    clickedPrice = e => {
-        
+  componentDidMount() {
+    Axios.get(`http://localhost:4000/api/sneakers/${this.state.id}`).then(
+      (res) => {
+        console.table(res);
+        this.setState({ sneaker: res.data });
+      }
+    );
+  }
+
+  navigateBack = (e) => {
+    navigate(-1);
+  };
+
+  clickedPrice = (e) => {
+    this.setState({ totalprice: e.target.value });
+
+    console.log(e.target.value);
+  };
+
+  render() {
+    return (
+      <div className="detail-wrapper">
+        <div className="detail-box">
+          <img
+            className="detail-img"
+            src={"./images/" + this.state.sneaker.filepath}
+            alt="sneaker image"
+          />
+          <p>{this.state.sneaker.name}</p>
+         <p>NZD $ {this.state.sneaker.newprice}</p>
+
+        </div>
+
+        <div className="comment">
+          <AddComment />
+          <CommentsWrapper />
+        </div>
+
       
-        this.setState({ totalprice: e.target.value });
+        <div className="select-size">
+          <div>
+            <span className="size">8.5</span>{" "}
+          </div>
+          <div>
+            <span className="size">9</span>{" "}
+          </div>
+          <div>
+            <span className="size">9.5</span>{" "}
+          </div>
+          <div>
+            <span className="size">10</span>{" "}
+          </div>
+          <div>
+            <span className="size">10.5</span>{" "}
+          </div>
+        </div>
+
       
-        console.log (e.target.value);
-        
-    }
 
-
-    render() {
-        return (
-            <div className="detail-wrapper">
-               
-               <div className="detail-box">
-                <img className="detail-img" src={"./images/" + this.state.sneaker.filepath} alt="sneaker image"/>
-                <p>{this.state.sneaker.name}</p>
-                </div>
-
-
-<div className="comment">
-
-
-    <CommentsWrapper/>
-    <AddComment/>
-</div>
-
-               <div className="select-price">
-
-               <label class="container">
-              
-  
- 
- <span>New Item: NZD $ {this.state.sneaker.newprice}</span>
- 
- <input type="radio" checked="checked" name="radio" value={this.state.sneaker.newprice} onChange={this.clickedPrice}/>
-</label>
-<label class="container">
-<span>Used Item: NZD $ {this.state.sneaker.oldprice}</span>
-  
-  
-  <input type="radio" name="radio" value={this.state.sneaker.oldprice} onChange={this.clickedPrice}/>
-</label>
-
-
-              
-               </div>
-               
-<div className="select-size">
-    
-    <div><span className="size">8.5</span> </div>
-    <div><span className="size">9</span>  </div>
-    <div><span className="size">9.5</span>  </div>
-    <div><span className="size">10</span>  </div>
-    <div><span className="size">10.5</span>  </div>
-
-    </div>
-
-<div className="total-price"
-value={this.state.totalprice}/>
-
-
-
-<div className="button-box">
-
-<SiteButton className="return" message="Back" action={this.navigateBack}/>
-                <SiteButton className="checkout" message="checkout"/>
-</div>
-                
-            </div>
-        )
-    }
+        <div className="button-box">
+          <SiteButton message="Back" action={this.navigateBack} style={whiteBtnStyle}/>
+          <SiteButton message="CheckOut" style={blackBtnStyle}/>
+        </div>
+      </div>
+    );
+  }
 }
